88e1c215f4d8ab1dd0ba11efb82dff2744566f75
chicagoist <31587029+chicagoist@users.noreply.github.com>
2021-01-04 14:45:45 +0200

    Update view.php

--- template/comp/view.php
+++ template/comp/view.php
@@ -1,31 +1,41 @@
 <!-- Компонентный подход. Шаблон гостевой книги. -->
-<html><head><title>Гостевая книга</title></head><body>
+<html><head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Гостевая книга</title>
+    </head>
+    <body>
 
-<!-- Блок ввода нового сообщения. -->
-<?php require_once "component_gbook_add.php" ?>
-<h1>Добавьте свое сообщение:</h1>
-<form method="post">
-  Ваше имя: <input type="text" name="new[name]"><br>
-  Комментарий:<br />
-  <textarea name="new[text]" cols="60" rows="5"></textarea><br>
-  <input type="submit" name="doAdd" value="Добавить!">
-</form>
+        <!-- Блок ввода нового сообщения. -->
+        <?php require_once 'component_gbook_add.php' ?>
 
-<!-- Блок сообщений гостевой книги. -->
-<?php require_once "component_gbook_show.php" ?>
-<h1>Сообщения гостевой книги:</h1>
-<?php foreach ($data as $id => $e) { ?>
-  Имя человека: <?= $e['name'] ?><br />
-  Его комментарий:<br /> <?= $e['text'] ?><hr />
-<?php } ?>
+        <h1>Добавьте свое сообщение:</h1>
+        <form method="post">
+            Ваше имя: <input type="text" name="new[name]"><br>
+            <?php if (@$data['error']['no_user_name']) { ?>
+                <span style="error">Не указано имя пользователя!</span>
+            <?php } ?>
+           <br /> Комментарий:<br />
+            <textarea name="new[text]" cols="60" rows="5"></textarea><br>
+            <?php if (@$data['error']['no_message_text']) { ?>
+                <span style="error">Не указан текст сообщения!</span>
+            <?php } ?>
+            <input type="submit" name="doAdd" value="Добавить!">
+        </form>
 
-<!-- Блок новостей. -->
-<h2>Последние новости:</h2>
-<?php require_once "component_news_show.php" ?>
-<ul>
-<?php foreach ($data as $i => $n) { ?>
-  <li><?= $i + 1 ?>-я новость: <?= $n ?></li>
-<?php } ?>
-</ul>
-
-</body></html>
+        <!-- Блок сообщений гостевой книги. -->
+        <?php require_once "component_gbook_show.php" ?>
+        <h1>Сообщения гостевой книги:</h1>
+        <?php foreach ($data as $id => $e) { ?>
+            Имя человека: <?= $e['name'] ?><br />
+            Его комментарий:<br /> <?= $e['text'] ?><hr />
+        <?php } ?>
+        <!-- Блок новостей. -->
+        <h2>Последние новости:</h2>
+        <?php require_once "component_news_show.php" ?>
+        <ul>
+            <?php foreach ($data as $i => $n) { ?>
+                <li><?= $i + 1 ?>-я новость: <?= $n ?></li>
+            <?php } ?>
+        </ul>
+    </body>
+</html>
